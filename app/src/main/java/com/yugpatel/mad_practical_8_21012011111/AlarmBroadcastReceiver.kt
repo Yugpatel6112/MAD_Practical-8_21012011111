77f8039ccd3f0a35d5ee5fa11eabdb6a3f732fb1
student <student@gnu.ac.in>
2023-09-29 08:45:07 +0530

    practical-8

---
+++ app/src/main/java/com/yugpatel/mad_practical_8_21012011111/AlarmBroadcastReceiver.kt
@@ -0,0 +1,25 @@
+package com.yugpatel.mad_practical_8_21012011111
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmBroadcastReceiver : BroadcastReceiver() {
+    companion object {
+        val ALARMKEY = "KEY"
+        val ALARMSTRAT = "START"
+        val ALARMSTOP = "STOP"
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        val DATA = intent.getStringExtra(ALARMKEY)
+        val intentservice=Intent(context,AlarmService::class.java)
+        if (DATA == ALARMSTRAT) {
+            context.startService(intentservice)
+        }
+        else{
+            context.stopService(intentservice)
+        }
+    }
+}
--- app/src/main/java/com/yugpatel/mad_practical_8_21012011111/MainActivity.kt
+++ app/src/main/java/com/yugpatel/mad_practical_8_21012011111/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.yugpatel.mad_practical_8_21012011111
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +10,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun setAlarm(militime:Long,action:String){
+        val intentalarm=Intent(applicationContext,AlarmBroadcastReceiver::class.java)
+        intentalarm.putExtra(AlarmBroadcastReceiver.ALARMKEY,action)
+        val pendingintent=PendingIntent.getBroadcast(applicationContext,4345,intentalarm,PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+        val manager=getSystemService(ALARM_SERVICE) as AlarmManager
+        if(action==AlarmBroadcastReceiver.ALARMSTRAT){
+            manager.setExact(AlarmManager.RTC_WAKEUP,militime,pendingintent)
+        }
+        else if(action==AlarmBroadcastReceiver.ALARMSTOP){
+            manager.cancel(pendingintent)
+            sendBroadcast(intentalarm)
+        }
+
     }
 }
